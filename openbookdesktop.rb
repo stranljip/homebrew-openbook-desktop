5d5321c89bf984da9afd551e311b8752c5901f7d
Carsten Heidmann <carsten@heidmann.info>
2019-05-04 11:24:20 +0200

    Create openbookdesktop.rb

---
+++ openbookdesktop.rb
@@ -0,0 +1,10 @@
+cask 'openbookdesktop' do
+  version '0.0.43-1'
+  sha256 'e7b90eb7b48c807b102fe3d3e1b8dfe5ce450d17c306a7e7f2039269d8a23767'
+
+  url "https://gitlab.com/999eagle/openbook-desktop/-/jobs/artifacts/#{version}/download?job=release-macos"
+  name 'openbook-desktop'
+  homepage 'https://gitlab.com/999eagle/openbook-desktop'
+
+  app 'OpenbookDesktop.app'
+end
