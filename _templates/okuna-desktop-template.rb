b0a90943e24acc3aff61e624a287f3e2d189c10a
Carsten Heidmann <carsten@heidmann.info>
2019-07-30 18:52:50 +0200

    Renaming ...

--- _templates/okuna-desktop-template.rb
+++ _templates/okuna-desktop-template.rb
@@ -3,7 +3,7 @@ cask 'okuna-desktop' do
   sha256 'ARTIFACTHASH'
 
   url "https://gitlab.com/999eagle/okuna-desktop/-/jobs/artifacts/#{version}/download?job=release-macos"
-  name 'openspace-desktop'
+  name 'okuna-desktop'
   homepage 'https://gitlab.com/999eagle/okuna-desktop'
 
   app 'OkunaDesktop.app'
