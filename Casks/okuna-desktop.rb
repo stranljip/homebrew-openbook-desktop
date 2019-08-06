9c16016394094d5aed0aee38f92380493d348067
Carsten Heidmann <carsten@heidmann.info>
2019-08-06 22:30:15 +0200

    New version 0.0.50-1

--- Casks/okuna-desktop.rb
+++ Casks/okuna-desktop.rb
@@ -1,6 +1,6 @@
 cask 'okuna-desktop' do
-  version '0.0.50'
-  sha256 'bba92f5959e02cf5b33ec04c21983324e80470d1cacf312a5ec35a53c73b9de0'
+  version '0.0.50-1'
+  sha256 '94e206298df1cbd36fbc146378574e995598a69d8596fd6475a577b32ad9522a'
 
   url "https://gitlab.com/999eagle/okuna-desktop/-/jobs/artifacts/#{version}/download?job=release-macos"
   name 'okuna-desktop'
