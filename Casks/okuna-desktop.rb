a0c5088078508574cfaca6604a267a43dc018a53
Carsten Heidmann <carsten@heidmann.info>
2019-09-21 20:03:50 +0200

    New version 0.0.52-hotfix.1

--- Casks/okuna-desktop.rb
+++ Casks/okuna-desktop.rb
@@ -1,6 +1,6 @@
 cask 'okuna-desktop' do
   version '0.0.52-hotfix.1'
-  sha256 'c8aa54d048cfa2055ae643926442f6af360d8c3e050dc811636ac0ae7e38c7e3'
+  sha256 'f6f38be55a7159b69bc08809b70ca58c32a9025ac09e0b2b21c009579e08580e'
 
   url "https://gitlab.com/999eagle/okuna-desktop/-/jobs/artifacts/#{version}/download?job=release-macos"
   name 'okuna-desktop'
