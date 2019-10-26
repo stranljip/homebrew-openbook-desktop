fb1970093f038d5867b168c52e2f225116419a43
Sophie Tauchert <999eagle@999eagle.moe>
2019-10-26 17:00:36 +0200

    Update to v0.0.55

--- Casks/okuna-desktop.rb
+++ Casks/okuna-desktop.rb
@@ -1,6 +1,6 @@
 cask 'okuna-desktop' do
-  version '0.0.52-hotfix.1'
-  sha256 'f6f38be55a7159b69bc08809b70ca58c32a9025ac09e0b2b21c009579e08580e'
+  version '0.0.55'
+  sha256 'cdeb18f34e8f0db46b591ccf30bdcd9f8130ab4c76706fdb25530c893a7e0ea5'
 
   url "https://gitlab.com/999eagle/okuna-desktop/-/jobs/artifacts/#{version}/download?job=release-macos"
   name 'okuna-desktop'
