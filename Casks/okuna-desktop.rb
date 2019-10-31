a623020f57afa59fa85a18b1f6d75f062eb1e726
Carsten Heidmann <carsten@heidmann.info>
2019-10-31 15:04:43 +0100

    Update Hash

--- Casks/okuna-desktop.rb
+++ Casks/okuna-desktop.rb
@@ -1,6 +1,6 @@
 cask 'okuna-desktop' do
   version '0.0.55'
-  sha256 'cdeb18f34e8f0db46b591ccf30bdcd9f8130ab4c76706fdb25530c893a7e0ea5'
+  sha256 '7fc086a88f6654c788fa40142733200ae1a2d3780703402912d04ecc3c405b00'
 
   url "https://gitlab.com/999eagle/okuna-desktop/-/jobs/artifacts/#{version}/download?job=release-macos"
   name 'okuna-desktop'
